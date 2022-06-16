2345ac339f92298bb31d5c7cc49ea377f4062717
Alexey Nepriyatelev <nepriyatelev@icloud.com>
2022-06-16 12:32:12 +0300

    update day03 ex00

--- Day03/ex00/Program.java
+++ Day03/ex00/Program.java
@@ -1,16 +1,36 @@
 public class Program {
-    public static void main(String[] args) {
-        int count = Integer.parseInt(args[0].substring(args[0].indexOf('=') + 1));
-        MyThread hen = new MyThread("Hen", count);
-        MyThread egg = new MyThread("Egg", count);
-        hen.start();
-        egg.start();
-        try {
-            hen.join();
-            egg.join();
-        } catch (InterruptedException ignored) {}
+    public static void main(String[] args) throws InterruptedException {
+        int count = parser(args);
+        Runnable runnable = () -> {
+            for (int i = 0; i < count; i++) {
+
+                +
+            }
+        };
+        Thread egg = new Thread(runnable, "egg");
+        Thread hen = new Thread(runnable, "hen");
+        egg.start(); hen.start();
+        egg.join(); hen.join();
         for (int i = 0; i < count; i++) {
             System.out.println("Human");
         }
     }
+
+    private static int parser(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Incorrect number of arguments!");
+            System.exit(-1);
+        }
+        if (!(args[0].substring(0, args[0].indexOf('=') + 1).equals("--count="))) {
+            System.err.println("Invalid argument name!");
+            System.exit(-1);
+        }
+        int count = Integer.parseInt(args[0].substring(args[0].indexOf('=') + 1));
+        if (count < 1) {
+            System.err.println("The counter is less than 1!");
+            System.exit(-1);
+        }
+        return count;
+    }
 }
+
